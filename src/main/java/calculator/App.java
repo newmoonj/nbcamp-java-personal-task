5652f1a2c776577fb0156229a0371f932d05cbb7
Robbie <robbie331@>
2025-07-12 13:44:01 +0900

    Level 1-5 trouble-1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -128,85 +128,113 @@ public class App {
 
         // 🧮 Level 1-5
         // 연산 결과 10개를 저장할 수 있는 배열 추가
-        int maxResults = 10; // 최대 결과 저장 개수(10개)
+        // 숫자 아닌 입력값 예외처리 추가
+// max
+        int maxResults = 3; // 최대 결과 저장 개수(10개)
         int[] results = new int[maxResults];
         int resultCnt = 0; // 저장된 결과 count(index)
 
         boolean running = true; // 반복 제어 플래그
 
         while (running) {
-            // 숫자 입력
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt(); // 적합한 타입으로 선언한 변수에 저장
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            int num1 = 0;
+            int num2 = 0;
 
-            // 연산자 입력
-            System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
-            char operator = sc.next().charAt(0);
-
-            // 계산 수행
-            int result = 0;
-            boolean validOperation = true; // 연산자 확인 플래그
-
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
+            // 첫번째 숫자 입력
+            while (true) {
+                try {
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    num1 = sc.nextInt(); // 적합한 타입으로 선언한 변수에 저장
                     break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 != 0) {
-                        result = num1 / num2;
-                    } else {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        validOperation = false;
-                    }
-                    break;
-                default:
-                    System.out.println("지원하지 않는 연산자입니다. +, -, *, / 중 하나를 입력해주세요.");
-                    validOperation = false;
-            }
-
-            // 연산이 정상적으로 됐을 때만 결과 출력
-            // 배열에 저장(최대 10개)
-            if (validOperation) {
-                System.out.println("결과: " + result);
-
-                if (resultCnt < maxResults) {
-                    results[resultCnt] = result; // 배열에 저장
-                    resultCnt++;
-
-                } else {
-                    System.out.printf("저장 공간이 가득 찼습니다. 저장한도: %d\n", maxResults);
+                } catch (Exception e) {
+                    System.out.println("잘못된 입력입니다. 숫자를 다시 입력해주세요.");
+                    sc.next();
                 }
             }
-            // 저장된 결과, 개수 출력
-            System.out.printf("[RESULT HISTORY(%d/%d)]\n", resultCnt, maxResults);
 
-            for (int i = 0; i < resultCnt; i++) {
-                System.out.print(results[i] + " ");
+            // 두번째 숫자 입력
+            while (true) {
+                try {
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    num2 = sc.nextInt();
+                    break;
+                } catch (Exception e) {
+                    System.out.println("잘못된 입력입니다. 숫자를 다시 입력해주세요.");
+                    sc.next();
+                }
             }
 
-            System.out.println();
+            // 연산자 입력 및 계산 수행
+            char operator;
+            boolean validOperation; // 연산자 확인 플래그
+            int result = 0;
 
-            // 반복 여부 확인
-            System.out.println("-".repeat(50));
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String exitInput = sc.next();
-            if (exitInput.equals("exit")) {
-                running = false;
+            do {
+                validOperation = true;
+                System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /): ");
+                operator = sc.next().charAt(0);
+
+                switch (operator) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 != 0) {
+                            result = num1 / num2;
+                        } else {
+                            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                            validOperation = false;
+                        }
+                        break;
+                    default:
+                        System.out.println("지원하지 않는 연산자입니다. +, -, *, / 중 하나를 입력해주세요.");
+                        validOperation = false;
+
+                }
+            } while (validOperation == false) ;
+
+
+                // 연산이 정상적으로 됐을 때만 결과 출력
+                // 배열에 저장(최대 10개)
+                if (validOperation) {
+                    System.out.println("결과: " + result);
+
+                    if (resultCnt < maxResults) {
+                        results[resultCnt] = result; // 배열에 저장
+                        resultCnt++;
+
+                    } else {
+                        System.out.printf("저장 공간이 가득 찼습니다. 저장한도: %d\n", maxResults);
+                    }
+                }
+                // 저장된 결과, 개수 출력
+                System.out.printf("[RESULT HISTORY(%d/%d)]\n", resultCnt, maxResults);
+
+                for (int i = 0; i < resultCnt; i++) {
+                    System.out.print(results[i] + " ");
+                }
+
+                System.out.println();
+
+                // 반복 여부 확인
+                System.out.println("-".repeat(50));
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                String exitInput = sc.next();
+                if (exitInput.equals("exit")) {
+                    running = false;
+                }
+
             }
-        }
-
-        System.out.println("프로그램을 종료합니다.");
+            System.out.println("프로그램을 종료합니다.");
 
 
+            // ===========
 
-    // ===========
-        }
     }
+}
